2ab37c6f3e92ebde1f9a8819e591ffe2919f7ed3
Alexander Klauer <Graf.Zahl@gmx.net>
2012-01-27 17:48:30 +0100

    Test

---
+++ src/main/java/memory/Main.java
@@ -0,0 +1,15 @@
+package memory;
+
+/**
+ * Main class.
+ */
+public class Main {
+	/**
+	 * Entry point.
+	 *
+	 * @param args Program arguments.
+	 */
+	public static void main( String... args ) {
+		System.out.println( "Test" );
+	}
+}
