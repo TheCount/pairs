28cc876b911efa2a75d9090270aacbcec16afc5a
Alexander Klauer <Graf.Zahl@gmx.net>
2012-01-27 19:03:12 +0100

    Rename app

--- src/main/java/memory/ui/MainWindow.java
+++ src/main/java/memory/ui/MainWindow.java
@@ -12,9 +12,9 @@ public class MainWindow extends JFrame {
 	 * Displays main window.
 	 */
 	public MainWindow() {
-		super( Message.get( "label-test" ).toString() );
+		super( Message.get( "label-concentration" ).toString() );
 		setDefaultCloseOperation( EXIT_ON_CLOSE );
-		JLabel label = new JLabel( Message.get( "label-test" ).toString() );
+		JLabel label = new JLabel( Message.get( "label-concentration" ).toString() );
 		getContentPane().add( label );
 
 		pack();
