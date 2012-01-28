08ff5d713cba5daded4d54ba09cbacd7ab2b3dc2
Alexander Klauer <Graf.Zahl@gmx.net>
2012-01-28 01:26:33 +0100

    Applying coding standards to ResourceBundleControl.java

--- src/main/java/memory/util/ResourceBundleControl.java
+++ src/main/java/memory/util/ResourceBundleControl.java
@@ -20,7 +20,6 @@ import java.util.ResourceBundle;
  * resources in UTF-8 format. Hence this class to workaround the problem.
  */
 public class ResourceBundleControl extends ResourceBundle.Control {
-
 	/**
 	 * Resource bundle format.
 	 */
@@ -33,48 +32,55 @@ public class ResourceBundleControl extends ResourceBundle.Control {
 
 	// default constructed
 	
-	public @Override List<String> getFormats(final String baseName) {
-		if (baseName == null)
-			throw new NullPointerException("Supplied baseName is null"); // per spec
-		return Collections.singletonList(FORMAT);
+	public @Override List<String> getFormats( final String baseName ) {
+		if ( baseName == null ) {
+			throw new NullPointerException( "Supplied baseName is null" ); // per spec
+		}
+		return Collections.singletonList( FORMAT );
 	}
 
-	public @Override ResourceBundle newBundle(final String baseName, final Locale locale, final String format,
-			final ClassLoader loader, final boolean reload)
-			throws IOException {
-		// throw some null pointer exceptions per spec
-		if (baseName == null)
-			throw new NullPointerException("Supplied baseName is null");
-		if (locale == null)
-			throw new NullPointerException("Supplied locale is null");
-		if (format == null)
-			throw new NullPointerException("Supplied format is null");
-		if (loader == null)
-			throw new NullPointerException("Supplied class loader is null");
-		// We do only properties
-		if (!FORMAT.equals(format))
+	public @Override ResourceBundle newBundle( final String baseName, final Locale locale, final String format, final ClassLoader loader, final boolean reload ) throws IOException {
+		/* throw some null pointer exceptions per spec */
+		if ( baseName == null ) {
+			throw new NullPointerException( "Supplied baseName is null" );
+		}
+		if ( locale == null ) {
+			throw new NullPointerException( "Supplied locale is null" );
+		}
+		if (format == null) {
+			throw new NullPointerException( "Supplied format is null" );
+		}
+		if ( loader == null ) {
+			throw new NullPointerException( "Supplied class loader is null" );
+		}
+		/* We do only properties */
+		if ( !FORMAT.equals(format) ) {
 			return null;
-		// open input stream for bundle data
-		final String bundleName = toBundleName(baseName, locale);
-		final String resourceName = toResourceName(bundleName, format); // possible NPE per spec
+		}
+		/* open input stream for bundle data */
+		final String bundleName = toBundleName( baseName, locale );
+		final String resourceName = toResourceName( bundleName, format ); // possible NPE per spec
 		InputStream stream;
-		if (reload) { // as suggested by API spec example
-			final URL url = loader.getResource(resourceName);
-			if (url == null)
+		if ( reload ) { // as suggested by API spec example
+			final URL url = loader.getResource( resourceName );
+			if ( url == null ) {
 				return null;
+			}
 			final URLConnection connection = url.openConnection();
-			if (connection == null)
+			if ( connection == null ) {
 				return null;
-			connection.setUseCaches(false);
+			}
+			connection.setUseCaches( false );
 			stream = connection.getInputStream();
 		} else {
-			stream = loader.getResourceAsStream(resourceName);
-			if (stream == null)
-			       return null;	
+			stream = loader.getResourceAsStream( resourceName );
+			if ( stream == null ) {
+			       return null;
+			}	       
 		}
-		// create and return bundle
-		final InputStreamReader reader = new InputStreamReader(stream, ENCODING);
-		final PropertyResourceBundle result = new PropertyResourceBundle(reader);
+		/* create and return bundle */
+		final InputStreamReader reader = new InputStreamReader( stream, ENCODING );
+		final PropertyResourceBundle result = new PropertyResourceBundle( reader );
 		reader.close();
 		return result;
 	}
