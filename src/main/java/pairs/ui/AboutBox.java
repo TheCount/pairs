e0c745b07b8343eefa8b1e79c2b6353a8108296e
Alexander Klauer <Graf.Zahl@gmx.net>
2012-01-31 23:24:04 +0100

    Empty about box

---
+++ src/main/java/pairs/ui/AboutBox.java
@@ -0,0 +1,49 @@
+/*
+    Pairs, a concentration game with modular card packages.
+    Copyright © 2012  Alexander Klauer
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package pairs.ui;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+import static pairs.util.Message._;
+
+/**
+ * About box.
+ */
+class AboutBox extends JDialog {
+	/**
+	 * Creates a new about box.
+	 *
+	 * @param owner Owner of the about box.
+	 */
+	AboutBox( Frame owner ) {
+		super( owner, _( "label-about" ) );
+		setDefaultCloseOperation( DISPOSE_ON_CLOSE );
+		setLayout( new BorderLayout() );
+		add( new JButton( new ButtonAction( "button-close" ) {
+			public void actionPerformed( ActionEvent e ) {
+				AboutBox.this.dispose();
+			}
+		} ), BorderLayout.SOUTH );
+		pack();
+		setVisible( true );
+	}
+}
---
+++ src/main/java/pairs/ui/ButtonAction.java
@@ -0,0 +1,43 @@
+/*
+    Pairs, a concentration game with modular card packages.
+    Copyright © 2012  Alexander Klauer
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package pairs.ui;
+
+/**
+ * Button action class.
+ */
+abstract class ButtonAction extends MenuItemAction {
+	/**
+	 * Creates a new button action.
+	 *
+	 * @param titleKey Action title key.
+	 * @param mnemoKey Action menmonic key.
+	 */
+	protected ButtonAction( String titleKey, String mnemoKey ) {
+		super( titleKey, mnemoKey, null, null, null );
+	}
+
+	/**
+	 * Creates a new button action with default parameters derived from the title key.
+	 *
+	 * @param titleKey Action title key.
+	 */
+	protected ButtonAction( String titleKey ) {
+		this( titleKey, titleKey + "-mnemo" );
+	}
+}
