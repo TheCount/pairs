6200dd2f420eaf5cfe026c3e16f49a366531ca44
Alexander Klauer <Graf.Zahl@gmx.net>
2012-02-05 14:14:42 +0100

    SVG icon through batik rasterizer

--- src/main/java/pairs/ui/MainWindow.java
+++ src/main/java/pairs/ui/MainWindow.java
@@ -20,10 +20,15 @@ package pairs.ui;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.*;
 
 import java.util.prefs.Preferences;
 
 import javax.swing.*;
+
+import org.apache.log4j.Logger;
+
+import pairs.util.SVGResource;
 
 import static pairs.util.Message._;
 
@@ -31,6 +36,11 @@ import static pairs.util.Message._;
  * Main window.
  */
 public class MainWindow extends JFrame implements ComponentListener, WindowStateListener {
+	/**
+	 * Logger for this class.
+	 */
+	private static final Logger logger = Logger.getLogger( MainWindow.class );
+
 	/**
 	 * Minimum window width.
 	 */
@@ -66,6 +76,30 @@ public class MainWindow extends JFrame implements ComponentListener, WindowState
 	 */
 	public MainWindow() {
 		super( _( "label-concentration" ) );
+
+		/* Program icon */
+		try {
+			SVGResource resource = new SVGResource( "images/Crystal_icons.svg" );
+			setIconImage( resource.createImage( 64, 64 ) );
+		} catch ( Exception e ) {
+			logger.error( _( "error-icon" ), e ); // FIXME: Define message
+		}
+		/* FIXME try {
+			TranscoderInput in = new TranscoderInput( ClassLoader.getSystemResourceAsStream( "images/Crystal_icons.svg" ) );
+			PNGTranscoder transcoder = new PNGTranscoder();
+			transcoder.transcode( in, null );
+			Image image = transcoder.createImage( 64, 64 );
+			setIconImage( image );
+			BufferedImage image = new BufferedImage( 64, 64, BufferedImage.TYPE_INT_ARGB );
+			Graphics2D graphics = image.createGraphics();
+			graphics.setClip( 0, 0, 64, 64 );
+			SVGUniverse svgUniverse = new SVGUniverse();
+			SVGDiagram diagram = svgUniverse.getDiagram( ClassLoader.getSystemResource( "images/Crystal_icons.svg" ).toURI(), true );
+			diagram.render( graphics );
+			setIconImage( image );
+		} catch ( Exception e ) {
+			logger.error( _( "error-icon" ), e ); // FIXME: Define message
+		} */
 
 		/* Default close operation */
 		setDefaultCloseOperation( EXIT_ON_CLOSE );
---
+++ src/main/java/pairs/util/SVGResource.java
@@ -0,0 +1,117 @@
+/*
+    Pairs, a concentration game with modular card packages.
+    Copyright © 2012  Alexander Klauer
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package pairs.util;
+
+import java.awt.*;
+import java.awt.image.*;
+
+import java.io.IOException;
+
+import java.net.URL;
+
+import java.util.MissingResourceException;
+
+import org.apache.batik.bridge.BridgeContext;
+import org.apache.batik.bridge.GVTBuilder;
+import org.apache.batik.bridge.UserAgentAdapter;
+import org.apache.batik.bridge.ViewBox;
+
+import org.apache.batik.dom.svg.SAXSVGDocumentFactory;
+
+import org.apache.batik.ext.awt.image.GraphicsUtil;
+
+import org.apache.batik.gvt.GraphicsNode;
+
+import org.apache.batik.util.XMLResourceDescriptor;
+
+import org.w3c.dom.Element;
+
+import org.w3c.dom.svg.SVGDocument;
+
+import static pairs.util.Message._;
+
+/**
+ * SVG resource convenience class.
+ */
+public class SVGResource {
+	/**
+	 * SVG DOM document.
+	 */
+	private final SVGDocument svgDocument;
+
+	/**
+	 * Root graphics node.
+	 */
+	private final GraphicsNode rootGraphicsNode;
+
+	/**
+	 * Creates a new SVG resource.
+	 *
+	 * @param resourceName Resource name.
+	 *
+	 * @throws MissingResourceException if a resource named resourceName does not exist.
+	 * @throws IOException if an error occurs while reading the resource.
+	 */
+	public SVGResource( String resourceName ) throws IOException {
+		URL resourceURL = ClassLoader.getSystemResource( resourceName );
+		if ( resourceURL == null ) {
+			throw new MissingResourceException( _( "error-loadingresource", resourceName ), ClassLoader.class.getName(), resourceName );
+		}
+		String parserClassName = XMLResourceDescriptor.getXMLParserClassName();
+		SAXSVGDocumentFactory documentFactory = new SAXSVGDocumentFactory( parserClassName );
+		this.svgDocument = (SVGDocument) documentFactory.createDocument( resourceURL.toString() );
+		this.rootGraphicsNode = new GVTBuilder().build( new BridgeContext( new UserAgentAdapter() ), this.svgDocument );
+	}
+
+	/**
+	 * Creates a new image.
+	 *
+	 * @param width Width of the new image.
+	 * @param height Height of the new image.
+	 * @param renderingHints Rendering hints for the new image.
+	 *
+	 * @return A new image rendered from SVG is returned.
+	 */
+	public Image createImage( int width, int height, RenderingHints renderingHints ) {
+		BufferedImage result = new BufferedImage( width, height, BufferedImage.TYPE_INT_ARGB );
+		Graphics2D graphics = GraphicsUtil.createGraphics( result );
+		graphics.addRenderingHints( renderingHints );
+
+		/* Render SVG to graphics */
+		graphics.transform( ViewBox.getViewTransform( null, svgDocument.getRootElement(), width, height ) );
+		rootGraphicsNode.paint( graphics );
+
+		/* return newly painted image */
+		graphics.dispose();
+		return result;
+	}
+
+	/**
+	 * Creates a new image with default rendering hints.
+	 *
+	 * @param width Width of the new image.
+	 * @param height Height of the new image.
+	 *
+	 * @return A new image rendered from SVG is returned.
+	 */
+	public Image createImage( int width, int height ) {
+		RenderingHints hints = new RenderingHints( RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON );
+		return createImage( width, height, hints );
+	}
+}
